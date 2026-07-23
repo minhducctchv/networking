2d3db4711da4d5efe0e66d6bbec41f7c5f21cfcc
minhducctchv <sieunhanbay1997@gmail.com>
2026-07-23 16:04:24 +0700

    update

--- content/data-link-layer/_meta.js
+++ content/data-link-layer/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: "Giới thiệu chương",
   "framing-and-mac-addressing": "Framing & MAC address",
   "ethernet-and-frames": "Ethernet & cấu trúc frame",
+  arp: "ARP: ánh xạ IP ↔ MAC",
   "switching-and-vlans": "Switching & VLAN",
   summary: "Tóm tắt chương",
 };
